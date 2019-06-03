2c291b4c71e4a6f02f913177df630516f15932a3
ottovbismarck <fritzj96@gmail.com>
2019-06-03 15:14:06 -0700

    initial commit

---
+++ src/components/App/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+
+import Pages from '../Pages';
+import Navigation from '../Navigation';
+
+import './styles.css';
+
+const App = () => (
+  <Router>
+    <div>
+      <Navigation />
+      <Switch>
+        <Route component={Pages.NoMatch} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+export default App;
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+const Navigation = () => (
+  <div className={styles.container}>
+    <div className=""></div>
+  </div>
+);
+
+export default Navigation;
