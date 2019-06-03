e80673dd092ae672cfdcc4f40103dbd1b2342c9a
ottovbismarck <fritzj96@gmail.com>
2019-06-03 16:49:47 -0700

    fiddling with css

--- src/components/Pages/NoMatch/index.js
+++ src/components/Pages/NoMatch/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import styles from './styles.module.css';
 
 const NoMatch = () => (
-  <div className={styles.container} />
+  <div className={styles.container}>
+    
+  </div>
 );
 
 export default NoMatch;
